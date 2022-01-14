d7f7bb98cc0a1229f8412179dea209c0f5896ae7
jb <jessb.epicodus@gmail.com>
2022-01-14 09:56:09 -0800

    add questions to html

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   $("form#triangle").submit(function(event) {
     event.preventDefault();
 
-    const input1 = ("input#q1").valueOf();
+    const input1 = parseInt($("input#q1").valueOf());
     const input2 = ("input#q2").valueOf();
     const input3 = ("input#q3").valueOf();
-    const input4 = ("input#q4").valueOf();
-    const input5 = ("input#q5").valueOf();
+    const input4 = $("#q4").valueOf();
+    const input5 = $("#q5").valueOf();
 
     $("#lang1").hide();
     $("#lang2").hide();
