f5b4e2ad542b6d368a126cf28bbac33a6ae0878d
jb <jessb.epicodus@gmail.com>
2022-01-14 16:20:29 -0800

    cleanup format; still error where a1 =0 & returns invalid response

--- js/scripts.js
+++ js/scripts.js
@@ -13,14 +13,15 @@ $(document).ready(function() {
     $("#lang3").hide();
     $("#invalid").hide();
 
-
     if ((a3 === 1) || (a4 === 1) || (a5 === 1)) {
       $("#invalid").show();
-    } else if (((a3 === a4) && (a3 === a5)) && (a1 && a2 >= 0)) {
+    } else if (((a3 === a4) && (a3 === a5)) && (a1 && a2 = 0)) {
       $("#lang1").show();
-    } else if (((a1 + a2 + a3 + a4 + a5) % 2 == 0) && (a1 && a2 >= 0)) {
+    } else if (((a3 === a4) && (a3 === a5)) && (a1 && a2 > 0)) {
+      $("#lang1").show();
+    } else if (((a1 + a2 + a3 + a4 + a5) % 2 == 0) && (a1 && a2 > 0)) {
       $("#lang2").show();
-    } else if (((a1 + a2 + a3 + a4 + a5) % 2 !== 0) && (a1 && a2 >= 0)) {
+    } else if (((a1 + a2 + a3 + a4 + a5) % 2 !== 0) && (a1 && a2 > 0)) {
       $("#lang3").show();
     } else {
       $("#invalid").show();
