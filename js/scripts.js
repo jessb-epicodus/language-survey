772daa6dbad7ab7289c49bb0f30bbac75932c354
jb <jessb.epicodus@gmail.com>
2022-01-21 07:58:53 -0800

    rename to less descriptive names

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
 
-    const lucky = parseInt($("input#question1").val());
-    const sib = parseInt($("input#question2").val());
-    const dayNi = parseInt($("#question3").val());
-    const fight = parseInt($("#question4").val());
-    const speak = parseInt($("#question5").val());
+    const ans1 = parseInt($("input#question1").val());
+    const ans2 = parseInt($("input#question2").val());
+    const ans3 = parseInt($("#question3").val());
+    const ans4 = parseInt($("#question4").val());
+    const ans5 = parseInt($("#question5").val());
 
     $("#lang1").hide();
     $("#lang2").hide();
@@ -14,13 +14,13 @@ $(document).ready(function() {
     $("#invalid").hide();
 
 //functions result in #invald when a1 = 0
-    if ((dayNi === 1) || (fight === 1) || (speak === 1)) {
+    if ((ans3 === 1) || (ans4 === 1) || (ans5 === 1)) {
       $("#invalid").show();
-    } else if (((dayNi === fight) && (dayNi === speak)) && ((lucky >= 0) && (sib >= 0)) {
+    } else if (((ans3 === ans4) && (ans3 === ans5)) && ((ans1 >= 0) && (ans2 >= 0)) {
       $("#lang1").show();
-    } else if (((lucky + sib + dayNi + fight + speak) % 2 == 0) && (lucky && sib >= 0)) {
+    } else if (((ans1 + ans2 + ans3 + ans4 + ans5) % 2 == 0) && (ans1 && ans2 >= 0)) {
       $("#lang2").show();
-    } else if (((lucky + sib + dayNi + fight + speak) % 2 !== 0) && (lucky && sib >= 0)) {
+    } else if (((ans1 + ans2 + ans3 + ans4 + ans5) % 2 !== 0) && (ans1 && ans2 >= 0)) {
       $("#lang3").show();
     } else {
       $("#invalid").show();
