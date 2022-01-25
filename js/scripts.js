f1dcf61390535a65c95c13f4e5c8dd3474f09acb
jb <jessb.epicodus@gmail.com>
2022-01-25 08:19:10 -0800

    fix so ans1 & ans2 can = 0; & fix incalid respose so all questions require answer.

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,13 @@ $(document).ready(function() {
 
     if ((ans3 === 1) || (ans4 === 1) || (ans5 === 1)) {
       $("#invalid").show();
+    } else if ((ans1 === "") || (ans2 === "")) {
+      $("#invalid").show();
     } else if (((ans3 === ans4) && (ans3 === ans5)) && ((ans1 >= 0) && (ans2 >= 0))) {
       $("#lang1").show();
-    } else if (((ans1 + ans2 + ans3 + ans4 + ans5) % 2 === 0) && (ans1 && ans2 >= 0)) {
+    } else if (((ans1 + ans2 + ans3 + ans4 + ans5) % 2 === 0) && ((ans1 >= 0) && (ans2 >= 0))) {
       $("#lang2").show();
-    } else if (((ans1 + ans2 + ans3 + ans4 + ans5) % 2 !== 0) && (ans1 && ans2 >= 0)) {
+    } else if (((ans1 + ans2 + ans3 + ans4 + ans5) % 2 !== 0) && ((ans1 >= 0) && (ans2 >= 0))) {
       $("#lang3").show();
     } else {
       $("#invalid").show();
